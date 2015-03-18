e9af1804e1a82ac1106d4bf91f7c1b0089ecfa2c
Gaurav Sarawgi <gaurav.sarawgi@outlook.com>
2015-03-18 17:33:42 +0530

    SampleWCF

---
+++ HelloWorldSolution/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldServiceNS
+{
+    public class HelloWorldService:IHelloWorldService
+    {
+        public string GetMessage(string Name)
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ HelloWorldSolution/HelloWorldService/IHelloWorldService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldServiceNS
+{
+    [ServiceContract]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        string GetMessage(string Name);
+    }
+}
