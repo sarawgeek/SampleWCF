6524cd375b0098db3032b743309120de1ebfdc83
Gaurav <sarawgeek@gmail.com>
2015-03-19 07:02:50 +0530

    Windows Service

---
+++ WCFHostedWindowsService/WCFHostedWindowsService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFHostedWindowsService
+{
+    public partial class WCFHostedWindowsService : ServiceBase
+    {
+        public ServiceHost serviceHost = null;
+
+        public WCFHostedWindowsService()
+        {
+            ServiceName = "WCFHelloService";
+        }
+        //public static void Main()
+        //{
+        //    ServiceBase.Run(new WCFHostedWindowsService());
+        //}
+
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost!=null)
+            {
+                serviceHost.Close();
+            }
+            serviceHost = new ServiceHost(typeof(WCFHostedWindowsService));
+            serviceHost.Open();
+
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+    }
+}
